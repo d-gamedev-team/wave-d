55f73abc69792fcffc508f9109c96dcf6ccbafef
ponce <aliloko@gmail.com>
2014-07-06 16:28:46 +0200

    Custom exception now follow "D Cookbook" guidelines.

--- waved/utils.d
+++ waved/utils.d
@@ -37,9 +37,9 @@ struct Sound
 /// The one type of Exception thrown in this library
 final class WavedException : Exception
 {
-    this(string msg)
+    this(string message, string file =__FILE__, size_t line = __LINE__, Throwable next = null)
     {
-        super(msg);
+        super(message, file, line, next);
     }
 }
 
